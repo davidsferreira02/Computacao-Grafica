404a1e859dae08ff408c5916cbc15239b3e57a89
David Ferreira <davidferreira@MacBook-Air-de-David-3.local>
2024-04-16 15:49:40 +0100

    Falta descobrir translate

--- project/MyFlower.js
+++ project/MyFlower.js
@@ -76,8 +76,7 @@ export class MyFlower extends CGFobject {
     for(var i=0;i<this.nrPetalas;i++){
       this.scene.pushMatrix();
     this.scene.rotate(-currentAngle, 1,0,1);
-    //this.scene.translate(this.raioCirc * Math.cos(currentAngle), this.tamanhoCaule + (this.raioCirc) , this.raioCirc * Math.sin(currentAngle));
-   this.scene.translate(this.tamanhoCaule + (this.raioCirc), this.raioCirc * Math.cos(currentAngle), this.raioCirc*10* Math.sin(currentAngle));
+    this.scene.translate(this.raioCirc * Math.cos(currentAngle), this.tamanhoCaule + (this.raioCirc) , this.raioCirc * Math.sin(currentAngle));
     this.petal.display();
     this.scene.popMatrix();
     currentAngle += angleIncrement;
